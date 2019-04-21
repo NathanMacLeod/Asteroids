7ccf07811f307a88ea8970aa79c1520e59629151
nathanmacleod <42585178+NathanMacLeod@users.noreply.github.com>
2019-04-20 22:27:04 -0700

    Update Asteroids.java

--- src/asteroids/Asteroids.java
+++ src/asteroids/Asteroids.java
@@ -1,9 +1,7 @@
 package asteroids;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * File added by Nathan MacLeod 2019
  */
 import java.awt.*;
 import java.awt.event.KeyAdapter;
